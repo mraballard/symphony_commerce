2247acdec8b6ac472b179b5d02a8851f5b205de7
Aaron Ballard <mraballard@gmail.com>
2016-12-15 16:10:24 -0300

    Removed unnecessary files, added scaffolding to index.html and app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+(function(){
+  angular.module('store')
+  .controller('mainController', MainController);
+
+  Maincontroller.$inject = ['$http'];
+
+  function MainController($http) {
+    var self = this;
+    
+  }
+
+})
