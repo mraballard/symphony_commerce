ab58ef4210f9e08ef3479f534b4fbe3b7ea9720e
Aaron Ballard <mraballard@gmail.com>
2016-12-15 16:22:37 -0300

    Added http get request to test angular controller. All working

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 (function(){
-  angular.module('store')
+  angular.module('store', [])
   .controller('mainController', MainController);
-
-  Maincontroller.$inject = ['$http'];
 
   function MainController($http) {
     var self = this;
-    
+    $http({
+      method: 'GET',
+      url: `https://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js`
+    })
+    .then(function(response){
+      console.log(response.data);
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
   }
 
-})
+})()
+console.log('app.js');
